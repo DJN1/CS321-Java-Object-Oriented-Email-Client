0924d35d0eee8bee46c7ca78ab9bb94c0f811b9d
Mylezalker <72838690+Mylezalker@users.noreply.github.com>
2021-03-21 10:15:48 -0500

    Add Manage Settings GUI for Project Phase 2
    NOTE: SettingsTester is a tester file with main() and is not to be included in final project build

---
+++ Manage Settings GUI/SettingsTester.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+import static com.mycompany.mavenproject1.ManageSites.ManageSitesPanel;
+import static com.mycompany.mavenproject1.ManageUsers.ManageUsersPanel;
+import static com.mycompany.mavenproject1.SwitchSettings.SwitchSettingsPanel;
+
+/**
+ *
+ * @author Zach
+ */
+public class SettingsTester 
+{
+    public static void main(String[] a) 
+    {
+        ManageSitesPanel();
+        ManageUsersPanel();
+        SwitchSettingsPanel();
+    }    
+}
