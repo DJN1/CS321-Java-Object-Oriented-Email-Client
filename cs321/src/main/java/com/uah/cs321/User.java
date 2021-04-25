55c86fe325fcadd6c81afffe0d5515c7170dfc4d
David Niederweis <david.niederweis@gmail.com>
2021-04-25 18:21:08 -0500

    update packages

--- cs321/src/main/java/com/uah/cs321/User.java
+++ cs321/src/main/java/com/uah/cs321/User.java
@@ -3,44 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.groupproject3.zachwalker;
+package com.uah.cs321;
 
 /**
  *
  * @author Zach
  */
 public class User {
-    
-    private final String userName;
-    private final String emailAddress;
-    private final String siteName;
-    private final Mailboxes userMailbox;
-    
-    public User(String aUserName, String aSiteName)
-    {
-        this.userName = aUserName;
-        this.siteName = aSiteName;
-        this.emailAddress = this.userName + "@" + this.siteName;
-        this.userMailbox = new Mailboxes(this.emailAddress);
-    }
-    
-    public String getUserName()
-    {
-        return this.userName;
-    }
-    
-    public String getEmailAddress()
-    {
-        return this.emailAddress;
-    }
-    
-    public String getSiteName()
-    {
-        return this.siteName;
-    }
-    
-    public Mailboxes getUserMailbox()
-    {
-        return this.userMailbox;
-    }
+
+	private final String userName;
+	private final String emailAddress;
+	private final String siteName;
+	private final Mailboxes userMailbox;
+
+	public User(String aUserName, String aSiteName) {
+		this.userName = aUserName;
+		this.siteName = aSiteName;
+		this.emailAddress = this.userName + "@" + this.siteName;
+		this.userMailbox = new Mailboxes(this.emailAddress);
+	}
+
+	public String getUserName() {
+		return this.userName;
+	}
+
+	public String getEmailAddress() {
+		return this.emailAddress;
+	}
+
+	public String getSiteName() {
+		return this.siteName;
+	}
+
+	public Mailboxes getUserMailbox() {
+		return this.userMailbox;
+	}
 }
