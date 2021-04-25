a11aa767d7193b3d133036b7bcb7e6d8cc95e7c6
David Niederweis <david.niederweis@gmail.com>
2021-04-25 18:26:22 -0500

    update packages

--- cs321/src/main/java/com/uah/cs321/Email.java
+++ cs321/src/main/java/com/uah/cs321/Email.java
@@ -1,5 +1,4 @@
-package com.mycompany.groupproject3.zachwalker;
-
+package com.uah.cs321;
 
 import java.util.Date;
 
@@ -8,50 +7,43 @@ import java.util.Date;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Zach
  */
 public class Email {
-    
-    private final String subject;
-    private final String body;
-    private final Date timeStamp;
-    private final String recipientName;
-    private final String senderName;
-    
-    public Email(String aSubject, String aBody, String aReceiver, String aSender)
-    {
-        this.subject = aSubject;
-        this.body = aBody;
-        this.timeStamp = new Date();
-        this.recipientName = aReceiver;
-        this.senderName = aSender;
-    }
-    
-    public String getSubject()
-    {
-        return this.subject;
-    }
-    
-    public String getBody()
-    {
-        return this.body;
-    }
-    
-    public Date getTimeStamp()
-    {
-        return this.timeStamp;
-    }
-    
-    public String getRecipient()
-    {
-        return this.recipientName;
-    }
-    
-    public String getSender()
-    {
-        return this.senderName;
-    }
+
+	private final String subject;
+	private final String body;
+	private final Date timeStamp;
+	private final String recipientName;
+	private final String senderName;
+
+	public Email(String aSubject, String aBody, String aReceiver, String aSender) {
+		this.subject = aSubject;
+		this.body = aBody;
+		this.timeStamp = new Date();
+		this.recipientName = aReceiver;
+		this.senderName = aSender;
+	}
+
+	public String getSubject() {
+		return this.subject;
+	}
+
+	public String getBody() {
+		return this.body;
+	}
+
+	public Date getTimeStamp() {
+		return this.timeStamp;
+	}
+
+	public String getRecipient() {
+		return this.recipientName;
+	}
+
+	public String getSender() {
+		return this.senderName;
+	}
 }
