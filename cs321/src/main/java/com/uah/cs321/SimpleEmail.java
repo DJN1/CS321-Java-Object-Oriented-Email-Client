6229f8a12e2e795d7d128bc898df1be1848df458
Mylezalker <72838690+Mylezalker@users.noreply.github.com>
2021-04-25 19:06:37 -0500

    Fixed error with siteList

--- cs321/src/main/java/com/uah/cs321/SimpleEmail.java
+++ cs321/src/main/java/com/uah/cs321/SimpleEmail.java
@@ -3,59 +3,61 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.uah.cs321;
-
+package com.mycompany.groupproject3.zachwalker;
 import java.util.ArrayList;
-
 /**
  *
  * @author Caleb
  */
-public class SimpleEmail {
+public class SimpleEmail 
+{
 
-	private static SimpleEmail simpleEmailInstance = null;
-
-	private ArrayList<Site> siteList;
-	private Site site;
-	private Site currentSite;
-	private SimpleEmail email;
-
-	private SimpleEmail() {
-		var siteList = new ArrayList<>();
-	}
-
-	public static SimpleEmail SimpleEmailInstance() {
-		if (simpleEmailInstance == null) {
-			simpleEmailInstance = new SimpleEmail();
-		}
-
-		return simpleEmailInstance;
-	}
-
-	public SimpleEmail getEmail() {
-		if (email == null) {
-			email = new SimpleEmail();
-		}
-		return email;
-	}
-
-	public void AddSite(Site site) {
-		siteList.add(site);
-		System.out.println("The site " + site.GetName() + " was added");
-
-	}
-
-	public void RemoveSite(Site site) {
-		siteList.remove(site);
-		System.out.println("The site " + site.GetName() + " was removed");
-	}
-
-	public void SelectSite(Site site) {
-		currentSite = site;
-		System.out.println("The site " + site.GetName() + " was selected");
-	}
-
-	public ArrayList<Site> getAllSites() {
-		return this.siteList;
-	}
+    private static SimpleEmail simpleEmailInstance = null;
+    
+    private ArrayList<Site> siteList;
+    private Site site;
+    private Site currentSite;
+    private SimpleEmail email;
+    
+    private SimpleEmail(){
+        siteList = new ArrayList<>();
+    }
+    
+    public static SimpleEmail getInstance()
+    {
+        if (simpleEmailInstance == null)
+        {
+            
+            simpleEmailInstance = new SimpleEmail();
+        }
+        
+        return simpleEmailInstance;
+    }
+    
+    public SimpleEmail getEmail() 
+    {
+        if (email == null)
+            email = new SimpleEmail();
+        return email;
+    }
+    
+    public void AddSite(Site site){
+        siteList.add(site);
+        System.out.println("The site " + site.GetName() + " was added"); 
+    }
+    
+    public void RemoveSite(Site site){
+        siteList.remove(site);
+        System.out.println("The site " + site.GetName() + " was removed");
+    }
+    
+    public void SelectSite(Site site){
+        currentSite = site;
+        System.out.println("The site " + site.GetName() + " was selected");
+    }
+    
+    public ArrayList<Site> getAllSites()
+    {
+        return this.siteList;
+    }
 }
