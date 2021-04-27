b096058cb881e2f370c22cb3759767f4bf48fcb2
David Niederweis <david.niederweis@gmail.com>
2021-04-26 21:47:57 -0500

    added MailBoxType enum to Mailboxes class and fixed formatting

--- cs321/src/main/java/com/uah/cs321/Mailboxes.java
+++ cs321/src/main/java/com/uah/cs321/Mailboxes.java
@@ -1,6 +1,5 @@
 package com.uah.cs321;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -9,94 +8,84 @@ import java.util.Collections;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+enum MailBoxType {
+	INBOX,
+	SENT,
+	TRASH
+}
 
 /**
  *
  * @author Zach
  */
 public class Mailboxes {
-    
-    private final String mailboxUserEmailAddress;
-    private final ArrayList<Email> sentBox;
-    private final ArrayList<Email> inbox;
-    private final ArrayList<Email> trashBox;
-    
-    public Mailboxes(String emailAddress)
-    {
-        this.mailboxUserEmailAddress = emailAddress;
-        this.sentBox = new ArrayList<>();
-        this.inbox = new ArrayList<>();
-        this.trashBox = new ArrayList<>();
-    }
-    
-    public String getMailboxUserEmailAddress()
-    {
-        return this.mailboxUserEmailAddress;
-    }
-    
-    public ArrayList<Email> getSentBox()
-    {
-        return this.sentBox;
-    }
-    
-    public ArrayList<Email> getInbox()
-    {
-        return this.inbox;
-    }
-    
-    public ArrayList<Email> getTrashBox()
-    {
-        return this.trashBox;
-    }
-    
-    public void sendEmailToTrash(Email aEmail, ArrayList<Email> emailBox)
-    {
-        var emailToMove = getEmailFromBox(aEmail, emailBox);
-        
-        this.trashBox.add(0, emailToMove);
-        
-        removeEmailFromBox(aEmail, emailBox);
-    }
-    
-    public void deleteEmailFromTrash(Email aEmail)
-    {
-        removeEmailFromBox(aEmail, this.trashBox);
-    }
-    
-    public void restoreEmailFromTrash(Email aEmail)
-    {
-        if (this.mailboxUserEmailAddress.equals(aEmail.getRecipient()))
-        {
-            this.inbox.add(aEmail);
-            sortEmailsDescByTimeStamp(inbox);
-        }
-        else
-        {
-            this.sentBox.add(aEmail);
-            sortEmailsDescByTimeStamp(sentBox);
-        }
-        
-        deleteEmailFromTrash(aEmail);
-    }
-    
-    public void emptyTrash()
-    {
-        this.trashBox.clear();
-    }
-    
-    private void removeEmailFromBox(Email aEmail, ArrayList<Email> emailBox)
-    {
-        var emailToRemove = emailBox.indexOf(getEmailFromBox(aEmail, emailBox));
-        emailBox.remove(emailToRemove);
-    }
-        
-    private Email getEmailFromBox(Email aEmail, ArrayList<Email> emailBox)
-    {
-        return emailBox.stream().filter(email -> email.getSubject().equals(aEmail.getSubject())).findFirst().orElse(null);
-    }
-    
-    private void sortEmailsDescByTimeStamp(ArrayList<Email> emailBox)
-    {
-        Collections.sort(emailBox, (Email e1, Email e2) -> e2.getTimeStamp().compareTo(e1.getTimeStamp()));
-    }
+
+	private final String mailboxUserEmailAddress;
+	private final ArrayList<Email> sentBox;
+	private final ArrayList<Email> inbox;
+	private final ArrayList<Email> trashBox;
+
+	public Mailboxes(String emailAddress) {
+		this.mailboxUserEmailAddress = emailAddress;
+		this.sentBox = new ArrayList<>();
+		this.inbox = new ArrayList<>();
+		this.trashBox = new ArrayList<>();
+	}
+
+	public String getMailboxUserEmailAddress() {
+		return this.mailboxUserEmailAddress;
+	}
+
+	public ArrayList<Email> getSentBox() {
+		return this.sentBox;
+	}
+
+	public ArrayList<Email> getInbox() {
+		return this.inbox;
+	}
+
+	public ArrayList<Email> getTrashBox() {
+		return this.trashBox;
+	}
+
+	public void sendEmailToTrash(Email aEmail, ArrayList<Email> emailBox) {
+		var emailToMove = getEmailFromBox(aEmail, emailBox);
+
+		this.trashBox.add(0, emailToMove);
+
+		removeEmailFromBox(aEmail, emailBox);
+	}
+
+	public void deleteEmailFromTrash(Email aEmail) {
+		removeEmailFromBox(aEmail, this.trashBox);
+	}
+
+	public void restoreEmailFromTrash(Email aEmail) {
+		if (this.mailboxUserEmailAddress.equals(aEmail.getRecipient())) {
+			this.inbox.add(aEmail);
+			sortEmailsDescByTimeStamp(inbox);
+		} else {
+			this.sentBox.add(aEmail);
+			sortEmailsDescByTimeStamp(sentBox);
+		}
+
+		deleteEmailFromTrash(aEmail);
+	}
+
+	public void emptyTrash() {
+		this.trashBox.clear();
+	}
+
+	private void removeEmailFromBox(Email aEmail, ArrayList<Email> emailBox) {
+		var emailToRemove = emailBox.indexOf(getEmailFromBox(aEmail, emailBox));
+		emailBox.remove(emailToRemove);
+	}
+
+	private Email getEmailFromBox(Email aEmail, ArrayList<Email> emailBox) {
+		return emailBox.stream().filter(email -> email.getSubject().equals(aEmail.getSubject())).findFirst().orElse(null);
+	}
+
+	private void sortEmailsDescByTimeStamp(ArrayList<Email> emailBox) {
+		Collections.sort(emailBox, (Email e1, Email e2) -> e2.getTimeStamp().compareTo(e1.getTimeStamp()));
+	}
 }
