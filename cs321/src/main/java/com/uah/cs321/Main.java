2c925892e0e247e442a8c231ca5a744a790135af
David Niederweis <david.niederweis@gmail.com>
2021-04-27 21:36:41 -0500

    added Tester test call

--- cs321/src/main/java/com/uah/cs321/Main.java
+++ cs321/src/main/java/com/uah/cs321/Main.java
@@ -14,6 +14,7 @@ public class Main {
 	public static MainFrame simpleEmailFrame = new MainFrame();
 
 	public static void main(String[] args) {
+		Tester.test();
 
 		//Opening GUI
 		simpleEmailFrame.openWindow();
