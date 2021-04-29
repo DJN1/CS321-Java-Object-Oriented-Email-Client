99d05bed7f7ec705fbf956b269753a87410b313d
David Niederweis <david.niederweis@gmail.com>
2021-04-28 19:52:44 -0500

    added more relevant example emails and users and sites

--- cs321/src/main/java/com/uah/cs321/Tester.java
+++ cs321/src/main/java/com/uah/cs321/Tester.java
@@ -2,31 +2,33 @@ package com.uah.cs321;
 
 /**
  *
- * @author David
+ * @author David, Caleb, Colin, Zach
  */
 public class Tester {
 
 	public static void test() {
 		Site gmail = new Site("gmail", "com");
-		Site yahoo = new Site("yahoo", "com");
-		User user1 = new User("test1", gmail);
-		User user2 = new User("test2", yahoo);
-//		for (int i = 0; i < 10; i++) {
-//			if (Math.random() > 0.5) {
-//				MailSender.getInstance().sendMessage(user2, user1.getEmailAddress(), new Email("subject" + i, "testing123", user1.getUserName(), user2.getUserName()));
-//			} else {
-//				MailSender.getInstance().sendMessage(user1, user2.getEmailAddress(), new Email("subject" + i, "testing123", user2.getUserName(), user1.getUserName()));
-//			}
-//		}
+		Site uah = new Site("uah", "edu");
 		SimpleEmail.getInstance().AddSite(gmail);
-		SimpleEmail.getInstance().AddSite(yahoo);
+		SimpleEmail.getInstance().AddSite(uah);
+		User rng = new User("random.user", gmail);
+		User zwu = new User("zmw0002", uah);
+		User dnu = new User("dn0039", uah);
+		User ccu = new User("cco0012", uah);
+		User cwu = new User("ckw0013", uah);
 		SimpleEmail.getInstance().SelectSite(gmail);
-		SimpleEmail.getInstance().GetCurrentSite().AddUser(user1);
-		SimpleEmail.getInstance().SelectSite(yahoo);
-		SimpleEmail.getInstance().GetCurrentSite().AddUser(user2);
-		SimpleEmail.getInstance().SelectSite(gmail);
-		SimpleEmail.getInstance().GetCurrentSite().SelectUser(user1);
-		Email u1tou2 = new Email("u1tou2", "testing123", user2, user1.getUserName());
-		MailSender.getInstance().sendMessage(user1, user2.getEmailAddress(), u1tou2);
+		SimpleEmail.getInstance().GetCurrentSite().AddUser(rng);
+		SimpleEmail.getInstance().SelectSite(uah);
+		SimpleEmail.getInstance().GetCurrentSite().AddUser(zwu);
+		SimpleEmail.getInstance().GetCurrentSite().AddUser(dnu);
+		SimpleEmail.getInstance().GetCurrentSite().AddUser(ccu);
+		SimpleEmail.getInstance().GetCurrentSite().AddUser(cwu);
+		Email dnu2zwu = new Email("final project", "Hey, can we work on the project tonight?\nDavid", dnu, zwu.getUserName());
+		MailSender.getInstance().sendMessage(dnu, zwu.getEmailAddress(), dnu2zwu);
+		Email ccu2cwu = new Email("design docs", "Hey,\n What's the status of the design documents?\nColin", ccu, cwu.getUserName());
+		MailSender.getInstance().sendMessage(ccu, cwu.getEmailAddress(), ccu2cwu);
+		Email spam = new Email("You are Millionare", "Hello Good Sir,\n I am nigerian prince and need your help.\nI have $50 millions dollares cash that I help to get to American.\nYou paypal me $100 dollares and I send you %10($5 millions) for your help.\nEmail me if you interested.\n\nPrince Neil of Nigeria", rng, dnu.getUserName()
+		);
+		MailSender.getInstance().sendMessage(rng, dnu.getEmailAddress(), spam);
 	}
 }
